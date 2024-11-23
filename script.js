ef137ce576cbe8d1bffae5eae73210adcc6b0bd9
Pranjul Upadhyay <pranjulupadhyay775@gmail.com>
2024-11-24 01:04:12 +0530

    fix: basic complete

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 function updateTime(){
     const now = new Date();
-    const hour = now.getHours();
-    const min = now.getMinutes();
-    const sec = now.getSeconds();
+    let hour = now.getHours();
+    const min = String(now.getMinutes()).padStart(2, '0');;
+    const sec = String(now.getSeconds()).padStart(2, '0');;
 
-    document.getElementsByClassName('time').textContent = `${hours}:${minutes}:${seconds}`;
+    //using amPm
+    const amPm = hour>=12? 'PM' : 'AM'; // Determine AM/PM
+    hour = hour%12 || 12; // Convert 0 (midnight) to 12, and 13-23 to 1-11
 
+    document.getElementById("time").textContent = `${hour}:${min}:${sec} ${amPm}`;
 }
+
+function showQuote(){
+    const quotes = [
+        "Time is keep GOING...So YOU also",
+        "Laga Reh Baawe",
+        "Time isn't STOPPING neither YOU",
+        "Ladke Laga Reh",
+        "Give BEST...F**k Rest",
+        "Arey Darr k Jiya to kiiya Jiya",
+        "Manzil ko Bhula k Jiya to kya Jiya",
+        "Jo Paani se nahaega vo LIBAAZ badalega...Jo Paseene se nahaega v ITIHAAS badalega"
+    ];
+
+    const randomIndex = Math.floor(Math.random()*quotes.length);
+    document.getElementById("quote").textContent = quotes[randomIndex];
+}
+//update time every sec
+setInterval(updateTime, 1000);
+
+//show quote
+showQuote();    
 
